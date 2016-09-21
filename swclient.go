084d2f3698d9db5f805543f03e132b4eaffd753c
Philip Gatzka <philipg92@live.de>
2016-09-21 18:33:12 +0200

    meh, interrupted

--- swclient.go
+++ swclient.go
@@ -1,1 +1,17 @@
 package swclient
+
+type swclient struct {
+	user    string
+	key     string
+	shopurl string
+	h       httpclient
+	hshr    Hash.Hash
+}
+
+func NewSWClient(user string, key string, shopurl string) *swclient {
+	return swclient{user: user, key: key, shopurl: shopurl, httpclient{}, md5.New()}
+}
+
+func (swc swclient) GetArticleById(id int) (string, error) {
+	return swc.h.get(shopurl+"/api/"+strconv.ItoA(id), swc.user, swc.key, swc.hshr)
+}
