9a68ae4b9f6135687fb1fdbc5098a64f5ede0d7a
philipgatzka <philipg92@live.de>
2016-10-25 15:51:15 +0200

    response model added

--- swclient.go
+++ swclient.go
@@ -3,6 +3,7 @@ package swclient
 import (
 	"bytes"
 	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"hash"
 	"io"
@@ -15,16 +16,17 @@ import (
 
 // TODO: higher-level functions like Put(id int, a article.Article)
 
+// Swclient defines the interface this library exposes
 type Swclient interface {
 	Resource(string) (*swclient, error)
-	Get() ([]byte, error)
-	GetById(id int) ([]byte, error)
-	PutById(id int, body io.Reader) ([]byte, error)
-	PostById(id int, body io.Reader) ([]byte, error)
-	DelById(id int) ([]byte, error)
+	Get() (*Response, error)
+	GetById(id int) (*Response, error)
+	PutById(id int, body io.Reader) (*Response, error)
+	PostById(id int, body io.Reader) (*Response, error)
+	DelById(id int) (*Response, error)
 }
 
-// swclient wraps the package and holds client and server information
+// swclient holds client and server information
 type swclient struct {
 	user     string
 	key      string
@@ -32,6 +34,12 @@ type swclient struct {
 	resource string
 	dgc      *digestclient
 	hshr     hash.Hash
+}
+
+// Response defines a response from shopware
+type Response struct {
+	Data    json.RawMessage `json:",omitempty"`
+	Success bool            `json:",omitempty"`
 }
 
 // New returns an initialised swclient
@@ -66,6 +74,7 @@ func New(user string, key string, apiurl string, resource string) (*swclient, er
 	}, nil
 }
 
+// Resource changes the api-resource swclient points to
 func (s *swclient) Resource(resource string) (*swclient, error) {
 	if len(resource) <= 0 {
 		return nil, errors.New("No resource specified")
@@ -74,28 +83,33 @@ func (s *swclient) Resource(resource string) (*swclient, error) {
 	return s, nil
 }
 
-func (s swclient) GetById(id int) ([]byte, error) {
+// GetById gets an object of the type specified in swclient.New() or swclient.Resource() from the shopware-api
+func (s swclient) GetById(id int) (*Response, error) {
 	return s.request("GET", strconv.Itoa(id), bytes.NewBufferString(""))
 }
 
-func (s swclient) Get() ([]byte, error) {
+// Get gets a list of objects of the type specified in swclient.New() or swclient.Resource() from the shopware-api
+func (s swclient) Get() (*Response, error) {
 	return s.request("GET", "", bytes.NewBufferString(""))
 }
 
-func (s swclient) PutById(id int, body io.Reader) ([]byte, error) {
+// PutById modifies an object of the type specified in swclient.New() or swclient.Resource() via the shopware-api
+func (s swclient) PutById(id int, body io.Reader) (*Response, error) {
 	return s.request("PUT", strconv.Itoa(id), body)
 }
 
-func (s swclient) PostById(id int, body io.Reader) ([]byte, error) {
+// PostById creates an object of the type specified in swclient.New() or swclient.Resource() via the shopware-api
+func (s swclient) PostById(id int, body io.Reader) (*Response, error) {
 	return s.request("POST", strconv.Itoa(id), body)
 }
 
-func (s swclient) DelById(id int) ([]byte, error) {
+// DelById deletes an object of the type specified in swclient.New() or swclient.Resource() via the shopware-api
+func (s swclient) DelById(id int) (*Response, error) {
 	return s.request("DELETE", strconv.Itoa(id), bytes.NewBufferString(""))
 }
 
-// request executes a request of the given method
-func (s swclient) request(method string, uri string, body io.Reader) ([]byte, error) {
+// request executes an http-request of the given method
+func (s swclient) request(method string, uri string, body io.Reader) (*Response, error) {
 	fullUri, err := s.constructUri(uri)
 	if err != nil {
 		return nil, err
@@ -106,17 +120,24 @@ func (s swclient) request(method string, uri string, body io.Reader) ([]byte, er
 		return nil, err
 	}
 
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return nil, errors.New(resp.Status)
+	// unmarshal received data into swclient.Response
+	data := Response{}
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		return nil, err
 	}
 
-	return b, nil
+	return &data, nil
 }
 
 func (s *swclient) constructUri(uri string) (string, error) {
