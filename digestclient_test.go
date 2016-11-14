a61a4986619b422b85bf380e2f7a39e6fbfc9199
philipgatzka <philipg92@live.de>
2016-11-14 12:27:49 +0100

    comments

--- digestclient_test.go
+++ digestclient_test.go
@@ -16,6 +16,7 @@ import (
 var mockResponseBody = `{"data":[{"id":1, "name": "Test"}], "success": true, "total": 1}`
 
 func TestRequest(t *testing.T) {
+	// Create local server for testing
 	ts, err := testDigestServer("401")
 	if err != nil {
 		t.Error(err)
@@ -26,9 +27,9 @@ func TestRequest(t *testing.T) {
 		dgst:  &digest{},
 		httpc: &http.Client{},
 	}
-
 	hshr := md5.New()
 
+	// Execute two requests
 	_, err = dgst.request("GET", ts.URL, bytes.NewBufferString(""), "user", "key", hshr)
 	if err != nil {
 		t.Error(err)
@@ -42,7 +43,8 @@ func TestRequest(t *testing.T) {
 	h := resp.Request.Header
 
 	auth := h.Get("Authorization")
-
+	// Check if the second response.request contains an Authorization header and the servers opaque value
+	// to confirm a successful first request and data retrieval
 	if auth == "" {
 		t.Error("digestclient didn't generate an authorised request!")
 	} else if !strings.Contains(auth, `opaque="thisisatest"`) {
@@ -51,13 +53,13 @@ func TestRequest(t *testing.T) {
 }
 
 func TestExec(t *testing.T) {
-
+	// Create a local server for testing
 	ts, err := testExecServer()
 	if err != nil {
 		t.Error(err)
 	}
 	defer ts.Close()
-
+	// Execute different requests and check if they were executed properly by examining the servers response
 	err = request("GET", ts.URL, bytes.NewBufferString(mockResponseBody+"get"), mockResponseBody+"get")
 	if err != nil {
 		t.Error(err)
@@ -108,6 +110,7 @@ func testExecServer() (*httptest.Server, error) {
 	return ts, nil
 }
 
+// request executes a request via digestclient.request() and checks the result
 func request(method string, uri string, body io.Reader, expected string) error {
 	req, err := http.NewRequest(method, uri, body)
 	if err != nil {
