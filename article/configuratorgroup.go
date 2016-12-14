92cc7e6d2588a9b78049638dc78cae7fad420a8e
philipgatzka <philipg92@live.de>
2016-12-14 18:50:37 +0100

    interrupted

--- article/configuratorgroup.go
+++ article/configuratorgroup.go
@@ -1,9 +1,8 @@
 package article
 
 type ConfiguratorGroup struct {
-	Id          int                  `json:"id,omitempty"`
-	Description string               `json:"description,omitempty"`
-	Name        string               `json:"name,omitempty"`
-	Position    int                  `json:"position,omitempty"`
-	Options     []ConfiguratorOption `json:"options,omitempty"`
+	Id          int    `json:"id,omitempty"`
+	Description string `json:"description,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Position    int    `json:"position,omitempty"`
 }
