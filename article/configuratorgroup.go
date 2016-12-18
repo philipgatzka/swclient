2de04d42a8e50b56009e9e949d8fb00a5d1f2d1e
philipgatzka <philipg92@live.de>
2016-12-18 14:41:04 +0100

    options

--- article/configuratorgroup.go
+++ article/configuratorgroup.go
@@ -1,8 +1,9 @@
 package article
 
 type ConfiguratorGroup struct {
-	Id          int    `json:"id,omitempty"`
-	Description string `json:"description,omitempty"`
-	Name        string `json:"name,omitempty"`
-	Position    int    `json:"position,omitempty"`
+	Id          int                  `json:"id,omitempty"`
+	Description string               `json:"description,omitempty"`
+	Name        string               `json:"name,omitempty"`
+	Position    int                  `json:"position,omitempty"`
+	Options     []ConfiguratorOption `json:"options,omitempty"`
 }
