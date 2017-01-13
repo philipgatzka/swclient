ed4e385a8e689f197b0618e5bc50c55a7b78c5d0
philipgatzka <philipg92@live.de>
2017-01-13 13:06:58 +0100

    added category path, removed bug workaround

--- article/category.go
+++ article/category.go
@@ -3,4 +3,5 @@ package article
 type Category struct {
 	Id   int    `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
+	Path string `json:"path,omitempty"`
 }
--- article/configuratoroption.go
+++ article/configuratoroption.go
@@ -1,34 +1,9 @@
 package article
 
-import (
-	"encoding/json"
-)
-
 type ConfiguratorOption struct {
 	Id       int    `json:"id,omitempty"`
 	GroupId  int    `json:"groupId,omitempty"`
 	Name     string `json:"name,omitempty"`
 	Position int    `json:"position,omitempty"`
 	Group    string `json:"group,omitempty"`
-}
-
-// MarshalJSON translates an article into JSON.
-// This is necessary, because the Shopware API returns a slightly different object on GETting than it expects on POSTing.
-// UPDATE: Had to leave "Name" AND "Option" in group due to a bug, got resolved: https://github.com/shopware/shopware/commit/76a58fc1a43600d8d6be5b3248a044fba84b8526
-func (o ConfiguratorOption) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Id       int    `json:"id,omitempty"`
-		GroupId  int    `json:"groupId,omitempty"`
-		Option   string `json:"option,omitempty"`
-		Name     string `json:"name,omitempty"`
-		Position int    `json:"position,omitempty"`
-		Group    string `json:"group,omitempty"`
-	}{
-		Id:       o.Id,
-		GroupId:  o.GroupId,
-		Option:   o.Name,
-		Name:     o.Name,
-		Position: o.Position,
-		Group:    o.Group,
-	})
 }
